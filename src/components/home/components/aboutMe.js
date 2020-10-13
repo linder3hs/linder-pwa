c47e10bd60b91dea84f75798b2f1f88db29ac1bf
linder3hs <linderhassinger00@gmail.com>
2020-10-13 00:44:56 -0500

    feat: design

---
+++ src/components/home/components/aboutMe.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function AboutMe() {
+
+    return (
+        <div>
+            <h2 className="text-white">Acerca de mi</h2>
+            <div className="about-me-text" style={{ textAlign: 'center' }}>
+                <div>
+                    <p className="about-me-text-style text-white">
+                        Hola soy Linder Hassinger, Software Developer me especializo en parte de Back - End, pero me
+                        gusta mucho React JS, así que a vece me pongo a jugar con un poco de Front, te mucho tiempo
+                        programado y tienen consultas puden escribirme a
+                        <a href="https://twitter.com/linder3hs" target="_blank" className="text-link">@linder3hs</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
